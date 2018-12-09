2543889c5b82bd6358af17be27b8a5a35e95c3ab
dinesh-ADT <dinesh.k@altradimension.com>
2018-12-10 00:37:44 +0530

    allowed hosts modified

--- settings/local.py
+++ settings/local.py
@@ -3,4 +3,4 @@ from .base import *
 DEBUG = True
 
 # ALLOWED_HOSTS = ['your_local_domain'] # or ['*']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
--- settings/production.py
+++ settings/production.py
@@ -3,4 +3,4 @@ from .base import *
 DEBUG = False
 
 # ALLOWED_HOSTS = ['your_production_domain_or_public_ip'] # or ['*']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ad-poc.herokuapp.com']
