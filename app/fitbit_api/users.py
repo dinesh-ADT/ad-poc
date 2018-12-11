a609166d28e9bf3d6bbe2d308913aac92e1a02e8
dinesh-ADT <dinesh.k@altradimension.com>
2018-12-11 15:08:36 +0530

    static users.py added

---
+++ app/fitbit_api/users.py
@@ -0,0 +1,14 @@
+user_data = {
+	"dinesh":{
+		"fitbit_id":"72DHHJ",
+		"code":"b435794852464cc51bbb5f480a7d36474413de34",
+		"access_token":"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRGNlQiLCJzdWIiOiI3MkRISEoiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNTQ0NTQ5MzQ5LCJpYXQiOjE1NDQ1MjA1NDl9.PypbuIXe7kf6g3-aKFGFf2gUWVZkVP07uOhMONahccc",
+		"refresh_token":"129fb160aa535a2a49eb6d842b14448cc942b8091548894e76209d973bddb838"
+		},
+	"pratik":{
+	"fitbit_id":"",
+	"code":"",
+	"access_token":"",
+	"refresh_token":""
+	}
+}
